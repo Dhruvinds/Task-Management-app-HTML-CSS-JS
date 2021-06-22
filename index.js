adcc2667860e0058a7b80d337b2e03041bef56c5
DhruvinDs <dhruvinsorathiya6464@gmail.com>
2021-06-22 14:34:38 +0530

    Add Card with JavaScript

---
+++ index.js
@@ -0,0 +1,44 @@
+const taskContainer = document.querySelector(".task__container");
+
+const generateNewCard = (taskData) => `
+<div class="col-md-6 col-lg-4" id=${taskData.id}>
+                <div class="card">
+                    <div class="card-header d-flex justify-content-end gap-2">
+                        <button type="button" class="btn btn-outline-success">
+                            <i class="fas fa-pencil-alt"></i></button>
+                        <button type="button" class="btn btn-outline-danger">
+                            <i class="fas fa-trash"></i></button>
+                    </div>
+                    <img src=${taskData.imageUrl} class="card-img-top" alt="...">
+
+
+                    <div class="card-body">
+                      <h5 class="card-title">${taskData.taskTitle}</h5>
+                      <p class="card-text">${taskData.taskDescription}</p>
+                      <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+                    </div>
+                  </div>
+            </div>
+`;
+
+const saveChanges = () => {
+    const taskData = {
+        id: `${Date.now()}`,  // Every second unique number for id
+        imageUrl: document.getElementById("imageurl").value,
+        taskTitle: document.getElementById("tasktitle").value,
+        taskType: document.getElementById("tasktype").value,
+        taskDescription: document.getElementById("taskdescription").value,
+    };
+    
+
+    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+};
+
+// Close the model after save ->1-line
+
+// Parent Object
+// Browser --> Window
+// DOM --->  Document
